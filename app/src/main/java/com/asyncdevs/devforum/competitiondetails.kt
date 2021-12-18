9f50170083874bd378e743bcddf6cb5c28c8ae09
Vijay A <vijaymangalani4588@gmail.com>
2021-12-18 10:37:40 +0530

    competition Details UI

---
+++ app/src/main/java/com/asyncdevs/devforum/competitiondetails.kt
@@ -0,0 +1,11 @@
+package com.asyncdevs.devforum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class competitiondetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_competitiondetails)
+    }
+}
