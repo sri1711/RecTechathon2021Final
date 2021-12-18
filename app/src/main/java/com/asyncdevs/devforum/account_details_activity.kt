3240a6b9abc6ba035bd1a06dbb60746f13018619
sri1711 <68659335+sri1711@users.noreply.github.com>
2021-12-18 11:05:26 +0530

    Account Details UI completed

---
+++ app/src/main/java/com/asyncdevs/devforum/account_details_activity.kt
@@ -0,0 +1,11 @@
+package com.asyncdevs.devforum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class account_details_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_details)
+    }
+}
